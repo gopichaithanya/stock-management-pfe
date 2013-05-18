0974117a2468990c9797b63d5e847ed89fb8fdb7
alexflik@gmail.com <alexflik@gmail.com@253cdd51-cfc9-1a29-8fd7-d5d73c15d4f2>
2013-05-18 18:21:28 +0000

    add some tests

--- trunk/stockManagement/test/com/pfe/InvoiceTest.java
+++ trunk/stockManagement/test/com/pfe/InvoiceTest.java
@@ -28,28 +28,34 @@ public class InvoiceTest {
 	@Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
 	public void receiveInvoiceTest() {
 
-		//Database connection
+		// Database connection
 		ApplicationContext context = new ClassPathXmlApplicationContext(
 				"db-config.xml");
 		SessionFactory sf = (SessionFactory) context.getBean("sessionFactory");
 		HibernateTemplate ht = new HibernateTemplate(sf);
-		
-		//Invoice attributes 
+
+		// Invoice attributes
 		Invoice i = new Invoice();
 		Date date = Calendar.getInstance().getTime();
-		i.setCode(20);
+		String paymentType = "onSale";
+		i.setCode(201);
 		i.setCreated(date);
-		i.setPaymentType("onSale");
+		i.setPaymentType(paymentType);
 		i.setRestToPay(new BigDecimal(0));
-		
-		//Shipments
+
+		// Shipments
 		List<Shipment> shipments = new ArrayList<Shipment>();
-		int qty = 300;
+		int qty = 1000;
 		Shipment s1 = new Shipment();
 		s1.setCreated(date);
 		s1.setCurrentQuantity(qty);
 		s1.setInitialQuantity(qty);
-		s1.setPaid(false);
+		if (paymentType.equals("onSale")) {
+			s1.setPaid(false);
+		} else {
+			s1.setPaid(true);
+		}
+
 		s1.setUnitPrice(new BigDecimal(20));
 		List<ProductType> types = ht
 				.find("from ProductType pt where pt.name = 'pen'");
@@ -58,69 +64,63 @@ public class InvoiceTest {
 		s1.setInvoice(i);
 		shipments.add(s1);
 		i.setShipments(shipments);
-		
-		//Supplier
+
+		// Supplier
 		List<Supplier> list = ht
 				.find("from Supplier s where s.name = 'Supplier 1'");
 		Supplier s = list.get(0);
 		i.setSupplier(s);
-		
-		//Update warehouse stocks
+
+		// Update warehouse stocks
 		List<Location> locations = ht
 				.find("from Location l where l.name = 'Main warehouse'");
 		Location warehouse = locations.get(0);
-		Stock stock = warehouse.getStockByType(type);
-		if(stock == null){
-			stock = new Stock();
-			stock.setType(type);
-			stock.setQuantity(qty);
-			warehouse.getStocks().add(stock);
-		} else{
-			int newQty = stock.getQuantity() + qty;
-			stock.setQuantity(newQty);
-		}
-		
+		warehouse.receiveProducts(type, qty);
+
 		ht.saveOrUpdate(warehouse);
 		ht.saveOrUpdate(i);
 	}
-	
+
 	@Test
 	@Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
 	public void deleteInvoiceTest() {
 
-		//Database connection
+		// Database connection
 		ApplicationContext context = new ClassPathXmlApplicationContext(
 				"db-config.xml");
 		SessionFactory sf = (SessionFactory) context.getBean("sessionFactory");
 		HibernateTemplate ht = new HibernateTemplate(sf);
-		
-		//Get the warehouse
+
+		// Get the warehouse
 		List<Location> locations = ht
 				.find("from Location l where l.name = 'Main warehouse'");
 		Location warehouse = locations.get(0);
-		
-		//Get invoice to delete
-		List<Invoice> list = ht.find("from Invoice i where i.code = 20");
-		Invoice i = list.get(0);
-		
-		List<Shipment> shipments = i.getShipments();
-		for(Shipment s : shipments){
-			ProductType pType = s.getProductType();
-			int qty = s.getCurrentQuantity();
-			Stock warehouseStock = warehouse.getStockByType(pType);
-			if(warehouseStock == null){
-				//can't delete ---> rollback everything
-			} else{
-				int warehouseQty = warehouseStock.getQuantity();
-				if(warehouseQty < qty){
-					//can't delete
-				} else{
-					int newQty = warehouseStock.getQuantity() - qty;
-					warehouseStock.setQuantity(newQty);
+
+		// Get invoice to delete
+		List<Invoice> list = ht.find("from Invoice i where i.code = 0");
+		if (list.size() > 0) {
+			Invoice i = list.get(0);
+
+			List<Shipment> shipments = i.getShipments();
+			// for each shipment to delete, remove products from
+			// warehouse stocks if possible
+			for (Shipment s : shipments) {
+				ProductType pType = s.getProductType();
+				int qty = s.getCurrentQuantity();
+				Stock warehouseStock = warehouse.getStockByType(pType);
+				if (warehouseStock == null) {
+					// can't delete ---> throw exception
+				} else {
+					int warehouseQty = warehouseStock.getQuantity();
+					if (warehouseQty < qty) {
+						// can't delete, throw exception
+					} else {
+						warehouse.removeProducts(pType, qty);
+					}
 				}
 			}
+			ht.delete(i);
+			ht.update(warehouse);
 		}
-		ht.delete(i);
-		ht.update(warehouse);
 	}
 }
--- trunk/stockManagement/test/com/pfe/LocationTest.java
+++ trunk/stockManagement/test/com/pfe/LocationTest.java
@@ -12,6 +12,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.pfe.server.model.Location;
 import com.pfe.server.model.LocationType;
+import com.pfe.server.model.ProductType;
+import com.pfe.server.model.Stock;
 
 @SuppressWarnings("unchecked")
 public class LocationTest {
@@ -29,11 +31,103 @@ public class LocationTest {
 		List<LocationType> list = ht
 				.find("from LocationType lt where lt.description = 'warehouse'");
 		LocationType locationType = list.get(0);
-		
+
 		Location w = new Location();
 		w.setName("Main warehouse");
 		w.setType(locationType);
 		ht.saveOrUpdate(w);
 
 	}
+
+	@Test
+	@Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+	public void moveStocksTest() {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"db-config.xml");
+		SessionFactory sf = (SessionFactory) context.getBean("sessionFactory");
+		HibernateTemplate ht = new HibernateTemplate(sf);
+
+		// we move products from the warehouse to location 1
+		// Get the warehouse
+		List<Location> warehouseList = ht
+				.find("from Location l where l.name = 'Main warehouse'");
+		Location warehouse = warehouseList.get(0);
+
+		// Get the location 1
+		List<Location> storeList = ht
+				.find("from Location l where l.name = 'Store 1'");
+		Location store = storeList.get(0);
+
+		// get productType to move
+		List<ProductType> types = ht
+				.find("from ProductType pt where pt.name = 'pen'");
+		ProductType type = types.get(0);
+		// quantity to move
+		int quantityToMove = 500;
+
+		Stock warehouseStock = warehouse.getStockByType(type);
+		if (warehouseStock == null) {
+			// throw exception; not enough goods
+		} else {
+			int availableQty = warehouseStock.getQuantity();
+			if (quantityToMove > availableQty) {
+				// throw exception; not enough goods
+			} else {
+				// update warehouse quantity
+				warehouse.removeProducts(type, quantityToMove);
+
+				// update store quantity
+				store.removeProducts(type, quantityToMove);
+
+				ht.update(warehouse);
+				ht.update(store);
+			}
+		}
+	}
+	
+	
+	@Test
+	@Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+	public void sellProductsTest() {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"db-config.xml");
+		SessionFactory sf = (SessionFactory) context.getBean("sessionFactory");
+		HibernateTemplate ht = new HibernateTemplate(sf);
+
+		//sell some products from the warehouse
+		// Get the warehouse
+		List<Location> warehouseList = ht
+				.find("from Location l where l.name = 'Main warehouse'");
+		Location warehouse = warehouseList.get(0);
+		
+		// get productType to sell
+		List<ProductType> types = ht
+				.find("from ProductType pt where pt.name = 'pen'");
+		ProductType type = types.get(0);
+		// quantity to sell
+		int quantityToSell = 20;
+		
+		Stock warehouseStock = warehouse.getStockByType(type);
+		if (warehouseStock == null) {
+			// throw exception; not enough goods
+		} else {
+			int availableQty = warehouseStock.getQuantity();
+			if (quantityToSell > availableQty) {
+				// throw exception; not enough goods
+			} else {
+				// update warehouse quantity
+				warehouse.removeProducts(type, quantityToSell);
+				// retrieve products from real shipments
+				
+				
+				
+				ht.update(warehouse);
+			
+			}
+		}
+		
+	}
+	
 }
