f2b3e11c619ca87d47db9ea696ebebf7f792216c
alexflik <alexflik@253cdd51-cfc9-1a29-8fd7-d5d73c15d4f2>
2013-06-03 09:34:45 +0000

    add edit invoice view

---
+++ stockManagement/src/com/pfe/client/mvp/presenters/InvoicePresenter.java
@@ -0,0 +1,11 @@
+package com.pfe.client.mvp.presenters;
+
+
+/**
+ * Controls the views displaying invoice related information
+ * 
+ * @author Alexandra
+ */
+public interface InvoicePresenter extends Presenter {
+
+}
