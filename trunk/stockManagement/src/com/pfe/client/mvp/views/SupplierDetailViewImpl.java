a08f51f8de2cabebe664ac84277c2d0d95d59868
alexflik <alexflik@253cdd51-cfc9-1a29-8fd7-d5d73c15d4f2>
2013-06-04 13:17:35 +0000

    add invoice details to supplier detail panel

--- trunk/stockManagement/src/com/pfe/client/mvp/views/SupplierDetailViewImpl.java
+++ trunk/stockManagement/src/com/pfe/client/mvp/views/SupplierDetailViewImpl.java
@@ -1,8 +1,11 @@
 package com.pfe.client.mvp.views;
 
+import java.util.ArrayList;
+
 import com.google.gwt.core.shared.GWT;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
+import com.pfe.shared.dto.InvoiceDTO;
 import com.pfe.shared.dto.SupplierDTO;
 import com.sencha.gxt.widget.core.client.ContentPanel;
 import com.sencha.gxt.widget.core.client.container.AccordionLayoutContainer;
@@ -15,6 +18,7 @@ public class SupplierDetailViewImpl implements SupplierDetailView {
 	private SupplierDTO supplier;
 	private Label descriptionLabel;
 	private Label nameLabel;
+	private Label invoiceLabel;
 
 	public SupplierDetailViewImpl(){
 		
@@ -22,7 +26,6 @@ public class SupplierDetailViewImpl implements SupplierDetailView {
 		AccordionLayoutAppearance appearance = GWT
 				.<AccordionLayoutAppearance> create(AccordionLayoutAppearance.class);
 		// details : Name tab
-		descriptionLabel = new Label();
 		nameLabel = new Label();
 		ContentPanel namePanel = new ContentPanel(appearance);
 		namePanel.setBodyStyleName("rawText");
@@ -31,14 +34,24 @@ public class SupplierDetailViewImpl implements SupplierDetailView {
 		namePanel.setExpanded(true);
 
 		// details : Description tab
+		descriptionLabel = new Label();
 		ContentPanel descPanel = new ContentPanel(appearance);
 		descPanel.setBodyStyleName("rawText");
 		descPanel.setHeadingText("Description");
 		descPanel.add(descriptionLabel);
 		descPanel.setExpanded(true);
 		
+		// details : Description tab
+		invoiceLabel = new Label();
+		ContentPanel invoicePanel = new ContentPanel(appearance);
+		invoicePanel.setBodyStyleName("rawText");
+		invoicePanel.setHeadingText("Invoices");
+		invoicePanel.add(invoiceLabel);
+		invoicePanel.setExpanded(true);
+		
 		detailsCon.add(namePanel);
 		detailsCon.add(descPanel);
+		detailsCon.add(invoicePanel);
 		detailsCon.setExpandMode(ExpandMode.MULTI);
 	}
 	
@@ -46,10 +59,23 @@ public class SupplierDetailViewImpl implements SupplierDetailView {
 	public Widget asWidget() {
 		nameLabel.setText(supplier.getName());
 		descriptionLabel.setText(supplier.getDescription());
-		//TODO add debt and invoice info
+		ArrayList<InvoiceDTO> invoices = supplier.getInvoices();
+		int size = invoices.size();
+		String text = "Total invoices : " + size + ". ";
+		if (size > 0) {
+			int debt = 0;
+			for (InvoiceDTO invoice : invoices) {
+				int restToPay = invoice.getRestToPay();
+				if (restToPay > 0) {
+					debt += restToPay;
+				}
+			}
+			text += " Total debt : " + debt + ".";
+		}
+		invoiceLabel.setText(text);
 		return detailsCon;
 	}
-
+	
 	@Override
 	public void clearData() {
 
