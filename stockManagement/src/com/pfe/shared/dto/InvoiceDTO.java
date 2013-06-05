d252d492968551ea3e67e1ce10d9f01cb0df5769
alexflik@gmail.com <alexflik@gmail.com@253cdd51-cfc9-1a29-8fd7-d5d73c15d4f2>
2013-06-05 17:27:35 +0000

    fix dto 3

---
+++ stockManagement/src/com/pfe/shared/dto/InvoiceDTO.java
@@ -0,0 +1,64 @@
+package com.pfe.shared.dto;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class InvoiceDTO implements Serializable {
+	
+	private Long id;
+	private int code;
+	private SupplierDTO supplier;
+	private String paymentType;
+	private ArrayList<ShipmentDTO> shipments;
+	private int restToPay;
+	private Date created;
+	
+	public InvoiceDTO(){
+		
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public SupplierDTO getSupplier() {
+		return supplier;
+	}
+	public void setSupplier(SupplierDTO supplier) {
+		this.supplier = supplier;
+	}
+	public String getPaymentType() {
+		return paymentType;
+	}
+	public void setPaymentType(String paymentType) {
+		this.paymentType = paymentType;
+	}
+	public ArrayList<ShipmentDTO> getShipments() {
+		return shipments;
+	}
+	public void setShipments(ArrayList<ShipmentDTO> shipments) {
+		this.shipments = shipments;
+	}
+	public int getRestToPay() {
+		return restToPay;
+	}
+	public void setRestToPay(int restToPay) {
+		this.restToPay = restToPay;
+	}
+	public Date getCreated() {
+		return created;
+	}
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+
+}
