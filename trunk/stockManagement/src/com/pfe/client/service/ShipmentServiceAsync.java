c54c119d2cae5817fae833ed4ee72540a80d254d
alexflik <alexflik@253cdd51-cfc9-1a29-8fd7-d5d73c15d4f2>
2013-06-07 08:45:18 +0000

    add service to delete list of shipments

--- trunk/stockManagement/src/com/pfe/client/service/ShipmentServiceAsync.java
+++ trunk/stockManagement/src/com/pfe/client/service/ShipmentServiceAsync.java
@@ -1,10 +1,14 @@
 package com.pfe.client.service;
 
+import java.util.List;
+
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.pfe.shared.dto.ShipmentDTO;
 
 public interface ShipmentServiceAsync {
 
 	void delete(ShipmentDTO shipment, AsyncCallback<Void> callback);
+
+	void deleteList(List<ShipmentDTO> shipments, AsyncCallback<Void> callback);
 
 }
--- trunk/stockManagement/src/com/pfe/server/service/ShipmentServiceImpl.java
+++ trunk/stockManagement/src/com/pfe/server/service/ShipmentServiceImpl.java
@@ -1,23 +1,42 @@
 package com.pfe.server.service;
 
+import java.util.List;
+
 import org.dozer.DozerBeanMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pfe.client.service.ShipmentService;
+import com.pfe.server.dao.shipment.ShipmentDao;
 import com.pfe.shared.BusinessException;
 import com.pfe.shared.dto.ShipmentDTO;
+import com.pfe.shared.model.Shipment;
 
 @Service("shipmentService") 
 public class ShipmentServiceImpl implements ShipmentService {
 	
 	@Autowired
+	private ShipmentDao dao;
+	@Autowired
 	private DozerBeanMapper dozerMapper;
 
 	@Override
 	public void delete(ShipmentDTO shipment) throws BusinessException {
-		// TODO Auto-generated method stub
+		
+		//TODO manage exceptions : if not enough quantity in warehouse stock => cannot delete
+		
+		Long id = shipment.getId();
+		Shipment entity = dozerMapper.map(shipment, Shipment.class);
+		//dao.delete(entity);
 
+	}
+
+	@Override
+	public void deleteList(List<ShipmentDTO> shipments) throws BusinessException {
+		
+		//TODO manage exceptions
+		System.out.println("server side");
+		
 	}
 
 }
