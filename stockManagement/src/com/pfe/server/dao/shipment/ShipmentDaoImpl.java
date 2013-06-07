64336a1fb6e3873268489332f5307fd7820f855a
alexflik <alexflik@253cdd51-cfc9-1a29-8fd7-d5d73c15d4f2>
2013-06-07 08:52:13 +0000

    add session factory to dao

--- stockManagement/src/com/pfe/server/dao/shipment/ShipmentDaoImpl.java
+++ stockManagement/src/com/pfe/server/dao/shipment/ShipmentDaoImpl.java
@@ -2,11 +2,21 @@ package com.pfe.server.dao.shipment;
 
 import java.util.List;
 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import com.pfe.server.dao.BaseDaoImpl;
 import com.pfe.shared.model.Shipment;
 
+@Repository
 public class ShipmentDaoImpl extends BaseDaoImpl<Long, Shipment> implements ShipmentDao {
 
+	@Autowired
+	public ShipmentDaoImpl(SessionFactory factory) {
+		super.setSessionFactory(factory);
+	}
+	
 	@Override
 	public void deleteList(List<Shipment> shipments) {
 		
