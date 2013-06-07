ecaeedeb9cb00bcacc88af6561a28537d447dc82
alexflik <alexflik@253cdd51-cfc9-1a29-8fd7-d5d73c15d4f2>
2013-06-07 14:08:30 +0000

    add deleteShipment method implementation

--- trunk/stockManagement/src/com/pfe/server/service/ShipmentServiceImpl.java
+++ trunk/stockManagement/src/com/pfe/server/service/ShipmentServiceImpl.java
@@ -1,5 +1,6 @@
 package com.pfe.server.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.dozer.DozerBeanMapper;
@@ -7,16 +8,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pfe.client.service.ShipmentService;
+import com.pfe.server.dao.location.LocationDAO;
+import com.pfe.server.dao.locationtype.LocationTypeDAO;
 import com.pfe.server.dao.shipment.ShipmentDao;
+import com.pfe.server.dao.stock.StockDAO;
 import com.pfe.shared.BusinessException;
 import com.pfe.shared.dto.ShipmentDTO;
+import com.pfe.shared.model.Location;
+import com.pfe.shared.model.LocationType;
+import com.pfe.shared.model.ProductType;
 import com.pfe.shared.model.Shipment;
+import com.pfe.shared.model.Stock;
 
 @Service("shipmentService") 
 public class ShipmentServiceImpl implements ShipmentService {
 	
 	@Autowired
-	private ShipmentDao dao;
+	private ShipmentDao shipmentDao;
+	@Autowired
+	private StockDAO stockDao;
+	@Autowired
+	private LocationDAO locationDao;
+	@Autowired
+	private LocationTypeDAO locationTypeDao;
 	@Autowired
 	private DozerBeanMapper dozerMapper;
 
@@ -34,9 +48,44 @@ public class ShipmentServiceImpl implements ShipmentService {
 	@Override
 	public void deleteList(List<ShipmentDTO> shipments) throws BusinessException {
 		
-		//TODO manage exceptions
-		System.out.println("server side");
+		List<Shipment> entities = new ArrayList<Shipment>();
+		for(ShipmentDTO shipment : shipments){
+			entities.add(dozerMapper.map(shipment, Shipment.class));
+		}
+
+		//Manage exceptions
+		for(Shipment entity : entities){
+			
+			//if part of the shipment was sold
+			if(entity.getCurrentQuantity() != entity.getInitialQuantity()){
+				throw new BusinessException("Some of the shipments are impossible to delete because" +
+						" items have been sold.");
+			}
+			
+			//if not enough goods in warehouse 
+			LocationType warehouseType = locationTypeDao.getWarehouseType();
+			Location warehouse = locationDao.get(warehouseType).get(0);	
+			ProductType type = entity.getProductType();
+			Stock stock = stockDao.get(warehouse, type);
+			if(stock == null){
+				throw new BusinessException("Some of the shipments are impossible to delete because" +
+						" there are not enought goods in the warehouse");
+			} else{
+				int availableQty = stock.getQuantity();
+				int shipmentQty = entity.getInitialQuantity();
+				if(availableQty < shipmentQty){
+					throw new BusinessException("Some of the shipments are impossible to delete because" +
+							" there are not enought goods in the warehouse");
+				}
+				else{
+					//delete shipment and update warehouse stocks
+					int remainingQty = availableQty - shipmentQty;
+					stock.setQuantity(remainingQty);
+					stockDao.merge(stock);
+					shipmentDao.delete(entity);
+				}
+			}
+		}
 		
 	}
-
 }
