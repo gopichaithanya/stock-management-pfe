5bbd06f6912a1807f9172b8bcf2fbf7913351053
alexflik <alexflik@253cdd51-cfc9-1a29-8fd7-d5d73c15d4f2>
2013-06-07 07:38:46 +0000

    add shipment daos

---
+++ trunk/stockManagement/src/com/pfe/server/dao/shipment/ShipmentDaoImpl.java
@@ -0,0 +1,9 @@
+package com.pfe.server.dao.shipment;
+
+import com.pfe.server.dao.BaseDaoImpl;
+import com.pfe.shared.model.Shipment;
+
+public class ShipmentDaoImpl extends BaseDaoImpl<Long, Shipment> implements ShipmentDao {
+	
+
+}
