adb2021701323550c44ceec96dec94a75b959d7e
alexflik <alexflik@253cdd51-cfc9-1a29-8fd7-d5d73c15d4f2>
2013-06-07 08:45:52 +0000

    add dao to delete list of shipments

--- stockManagement/src/com/pfe/server/dao/shipment/ShipmentDao.java
+++ stockManagement/src/com/pfe/server/dao/shipment/ShipmentDao.java
@@ -1,8 +1,16 @@
 package com.pfe.server.dao.shipment;
 
+import java.util.List;
+
 import com.pfe.server.dao.IBaseDao;
 import com.pfe.shared.model.Shipment;
 
 public interface ShipmentDao extends IBaseDao<Long, Shipment> {
 
+	/**
+	 * Deletes shipmenst
+	 * 
+	 * @param shipments
+	 */
+	public void deleteList(List<Shipment> shipments);
 }
