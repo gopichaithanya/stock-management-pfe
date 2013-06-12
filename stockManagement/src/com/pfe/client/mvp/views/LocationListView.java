ffcd3e129d30453ae729bdfe9aec610114108d6a
alexflik <alexflik@253cdd51-cfc9-1a29-8fd7-d5d73c15d4f2>
2013-06-12 14:00:58 +0000

    display locations with paging

--- stockManagement/src/com/pfe/client/mvp/views/LocationListView.java
+++ stockManagement/src/com/pfe/client/mvp/views/LocationListView.java
@@ -1,7 +1,14 @@
 package com.pfe.client.mvp.views;
 
+import java.util.List;
+
 import com.google.gwt.user.client.ui.IsWidget;
 import com.pfe.client.mvp.presenters.LocationPresenter;
+import com.pfe.shared.dto.LocationDTO;
+import com.sencha.gxt.data.shared.ListStore;
+import com.sencha.gxt.data.shared.loader.FilterPagingLoadConfig;
+import com.sencha.gxt.data.shared.loader.PagingLoadResult;
+import com.sencha.gxt.data.shared.loader.PagingLoader;
 
 public interface LocationListView extends IsWidget {
 	
@@ -11,5 +18,27 @@ public interface LocationListView extends IsWidget {
 	 * @param presenter
 	 */
 	public void setPresenter(LocationPresenter presenter);
+	
+	/**
+	 * Sets data to be rendered
+	 * 
+	 * @param locations
+	 */
+	public void setData(List<LocationDTO> locations);
+	
+	/**
+	 * Gets the view data
+	 * 
+	 * @return locations displayed
+	 */
+	public ListStore<LocationDTO> getData();
+	
+	/**
+	 * Set loader for paging
+	 * 
+	 * @param loader
+	 */
+	public void setPagingLoader(
+			PagingLoader<FilterPagingLoadConfig, PagingLoadResult<LocationDTO>> remoteLoader);
 
 }
