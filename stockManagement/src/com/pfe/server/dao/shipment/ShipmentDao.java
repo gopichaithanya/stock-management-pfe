babf748b1017f4fb60211b7399f5a677f2b640b9
alexflik <alexflik@253cdd51-cfc9-1a29-8fd7-d5d73c15d4f2>
2013-06-13 14:05:54 +0000

    add search method in shipment dao

--- stockManagement/src/com/pfe/server/dao/shipment/ShipmentDao.java
+++ stockManagement/src/com/pfe/server/dao/shipment/ShipmentDao.java
@@ -3,14 +3,27 @@ package com.pfe.server.dao.shipment;
 import java.util.List;
 
 import com.pfe.server.dao.IBaseDao;
+import com.pfe.shared.model.ProductType;
 import com.pfe.shared.model.Shipment;
 
 public interface ShipmentDao extends IBaseDao<Long, Shipment> {
 
 	/**
-	 * Deletes shipmenst
+	 * Deletes shipments
 	 * 
 	 * @param shipments
 	 */
 	public void deleteList(List<Shipment> shipments);
+	
+	/**
+	 * Retrieves shipments from start index where product type like parameter.
+	 * The number if records retrieved is equal to limit parameter. Type is
+	 * ignored if null.
+	 * 
+	 * @param start
+	 * @param limit
+	 * @param type
+	 * @return list of found shipments
+	 */
+	public List<Shipment> search(int start, int limit, ProductType type);
 }
--- stockManagement/src/com/pfe/server/dao/shipment/ShipmentDaoImpl.java
+++ stockManagement/src/com/pfe/server/dao/shipment/ShipmentDaoImpl.java
@@ -3,10 +3,13 @@ package com.pfe.server.dao.shipment;
 import java.util.List;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.pfe.server.dao.BaseDaoImpl;
+import com.pfe.shared.model.ProductType;
 import com.pfe.shared.model.Shipment;
 
 @Repository
@@ -23,6 +26,12 @@ public class ShipmentDaoImpl extends BaseDaoImpl<Long, Shipment> implements Ship
 		for(Shipment shipment : shipments){
 			delete(shipment);
 		}
+	}
+
+	@Override
+	public List<Shipment> search(int start, int limit, ProductType type) {
+		Criterion criterion = Restrictions.eq("productType", type);
+		return findByCriteria(start, limit, criterion);
 	}
 	
 
