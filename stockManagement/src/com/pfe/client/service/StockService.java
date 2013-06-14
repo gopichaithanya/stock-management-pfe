397e01dc42ce5cc17f31a490dfe3f3be74a89277
alexflik <alexflik@253cdd51-cfc9-1a29-8fd7-d5d73c15d4f2>
2013-06-14 12:21:38 +0000

    add ship method in stock service

--- stockManagement/src/com/pfe/client/service/StockService.java
+++ stockManagement/src/com/pfe/client/service/StockService.java
@@ -3,6 +3,7 @@ package com.pfe.client.service;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 import com.pfe.shared.BusinessException;
+import com.pfe.shared.dto.LocationDTO;
 import com.pfe.shared.dto.StockDTO;
 
 /**
@@ -19,7 +20,19 @@ public interface StockService extends RemoteService {
 	 * 
 	 * @param stock
 	 * @param quantity
-	 * @return stock with updated quantity
+	 * @return stock with updated quantity or null if total quantity sold
 	 */
 	public StockDTO sell(StockDTO stock, int quantity) throws BusinessException;
+
+	/**
+	 * Ships products from given stock to another location
+	 * 
+	 * @param stock
+	 * @param quantity
+	 * @param destination
+	 * @return stock with updated quantity or null if total quantity moved
+	 * @throws BusinessException
+	 */
+	public StockDTO ship(StockDTO stock, int quantity, LocationDTO destination)
+			throws BusinessException;
 }
--- stockManagement/src/com/pfe/client/service/StockServiceAsync.java
+++ stockManagement/src/com/pfe/client/service/StockServiceAsync.java
@@ -1,10 +1,14 @@
 package com.pfe.client.service;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.pfe.shared.dto.LocationDTO;
 import com.pfe.shared.dto.StockDTO;
 
 public interface StockServiceAsync {
 
 	void sell(StockDTO stock, int quantity, AsyncCallback<StockDTO> callback);
+
+	void ship(StockDTO stock, int quantity, LocationDTO destination,
+			AsyncCallback<StockDTO> callback);
 
 }
