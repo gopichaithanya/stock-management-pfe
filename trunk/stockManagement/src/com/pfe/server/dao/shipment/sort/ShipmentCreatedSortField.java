3c4a5e7fcb43920098fac1b0c364ace364cc9bc6
danboncioaga@gmail.com <danboncioaga@gmail.com@253cdd51-cfc9-1a29-8fd7-d5d73c15d4f2>
2013-06-16 13:54:39 +0000

    add demo for SortField (created date for Shipment)

---
+++ trunk/stockManagement/src/com/pfe/server/dao/shipment/sort/ShipmentCreatedSortField.java
@@ -0,0 +1,18 @@
+package com.pfe.server.dao.shipment.sort;
+
+public class ShipmentCreatedSortField extends ShipmentSortField{
+
+	private static final long serialVersionUID = -5698757481184832679L;
+
+	// Attribute from the model Entity on which to apply the sort
+	private static final String ENTITY_SORT_COLUMN = "createDate";
+	
+	public ShipmentCreatedSortField() {
+		this(true);
+	}
+	
+	public ShipmentCreatedSortField(boolean ascending) {
+		super(ENTITY_SORT_COLUMN, ascending);
+	}
+
+}
---
+++ trunk/stockManagement/src/com/pfe/server/dao/shipment/sort/ShipmentSortField.java
@@ -0,0 +1,13 @@
+package com.pfe.server.dao.shipment.sort;
+
+import com.pfe.server.dao.SortField;
+
+public class ShipmentSortField extends SortField{
+
+	private static final long serialVersionUID = -6854663208156337420L;
+	
+	public ShipmentSortField(String field, boolean ascending) {
+		super(field, ascending);
+	}
+	
+}
