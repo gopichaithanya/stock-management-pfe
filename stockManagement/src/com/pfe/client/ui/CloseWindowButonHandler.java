851fe18498248673d71474e8c605f7e1a6bb964e
alexflik <alexflik@253cdd51-cfc9-1a29-8fd7-d5d73c15d4f2>
2013-06-20 12:22:34 +0000

    add classes for view constants and handler for closing windows

---
+++ stockManagement/src/com/pfe/client/ui/CloseWindowButonHandler.java
@@ -0,0 +1,25 @@
+package com.pfe.client.ui;
+
+import com.sencha.gxt.widget.core.client.Window;
+import com.sencha.gxt.widget.core.client.event.SelectEvent;
+import com.sencha.gxt.widget.core.client.event.SelectEvent.SelectHandler;
+
+/**
+ * This handler is added to close buttons to hide windows
+ * 
+ * @author Alexandra
+ *
+ */
+public class CloseWindowButonHandler implements SelectHandler {
+	
+	private Window w;
+	public CloseWindowButonHandler(Window w){
+		this.w = w;
+	}
+	
+	@Override
+	public void onSelect(SelectEvent event) {
+		w.hide();
+	}
+
+}
