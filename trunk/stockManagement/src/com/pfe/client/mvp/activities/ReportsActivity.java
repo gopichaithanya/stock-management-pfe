f5dcf50d4abc4f1bada2c8c76d8b93b4c5c80a78
alexflik <alexflik@253cdd51-cfc9-1a29-8fd7-d5d73c15d4f2>
2013-06-21 10:00:19 +0000

    add stock report in reports view

--- trunk/stockManagement/src/com/pfe/client/mvp/activities/ReportsActivity.java
+++ trunk/stockManagement/src/com/pfe/client/mvp/activities/ReportsActivity.java
@@ -1,28 +1,76 @@
 package com.pfe.client.mvp.activities;
 
+import java.util.List;
+
 import com.google.gwt.activity.shared.AbstractActivity;
 import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.AcceptsOneWidget;
+import com.pfe.client.mvp.ClientFactory;
 import com.pfe.client.mvp.presenters.ReportsPresenter;
+import com.pfe.client.mvp.views.ReportsView;
+import com.pfe.client.service.ProductTypeServiceAsync;
+import com.pfe.client.service.StockServiceAsync;
 import com.pfe.shared.dto.ProductTypeDTO;
+import com.pfe.shared.dto.StockDTO;
 
 public class ReportsActivity extends AbstractActivity implements ReportsPresenter {
+	
+	private ClientFactory clientFactory;
+	private StockServiceAsync stockService;
+	private ProductTypeServiceAsync productTypeService;
+	
+	private ReportsView view;
+	
+	public ReportsActivity(ClientFactory clientFactory){
+		this.clientFactory = clientFactory;
+		this.stockService = clientFactory.getStockService();
+		this.productTypeService = clientFactory.getProductTypeService();
+	}
 
 	@Override
 	public void bind() {
-		// TODO Auto-generated method stub
+		view.setPresenter(this);
 
 	}
 
 	@Override
-	public void start(AcceptsOneWidget panel, EventBus eventBus) {
-		// TODO Auto-generated method stub
-
+	public void start(final AcceptsOneWidget panel, EventBus eventBus) {
+		view = clientFactory.getReportsView();
+		bind();
+		productTypeService.getAll(new AsyncCallback<List<ProductTypeDTO>>() {
+			
+			@Override
+			public void onSuccess(List<ProductTypeDTO> result) {
+				view.setProductTypes(result);
+				panel.setWidget(view.asWidget());
+				
+			}
+			
+			@Override
+			public void onFailure(Throwable caught) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 	}
 
 	@Override
 	public void getStocks(ProductTypeDTO productType) {
-		// TODO Auto-generated method stub
+		stockService.find(productType, new AsyncCallback<List<StockDTO>>() {
+			
+			@Override
+			public void onSuccess(List<StockDTO> result) {
+				view.setStocks(result);
+				
+			}
+			
+			@Override
+			public void onFailure(Throwable caught) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 
 	}
 
