459e193effd19a9c25f7808ac04da9f6fdf3e86a
alexflik <alexflik@253cdd51-cfc9-1a29-8fd7-d5d73c15d4f2>
2013-06-27 12:15:55 +0000

    add technical error message

--- trunk/stockManagement/src/com/pfe/client/ui/ViewConstants.java
+++ trunk/stockManagement/src/com/pfe/client/ui/ViewConstants.java
@@ -5,4 +5,6 @@ public class ViewConstants {
 	//Paging constants
 	public static final int recordsPerPage = 4;
 	public static final int pagingBarHeight = 35;
+	
+	public static final String techErrorMessage = "Unable to complete the operation, please contact the sysadmin.";
 }
