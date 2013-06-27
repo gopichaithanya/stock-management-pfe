4ae1b78eca0b6f9ebcdffb7d1faa321c3436f7ed
alexflik <alexflik@253cdd51-cfc9-1a29-8fd7-d5d73c15d4f2>
2013-06-27 07:15:59 +0000

    sort location stocks by product type name

--- trunk/stockManagement/src/com/pfe/shared/model/Stock.java
+++ trunk/stockManagement/src/com/pfe/shared/model/Stock.java
@@ -1,5 +1,7 @@
 package com.pfe.shared.model;
 
+import java.util.Comparator;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -60,4 +62,31 @@ public class Stock {
 	public void setLocation(Location location) {
 		this.location = location;
 	}
+	
+	/**
+	 * Compares two Stock entities on the basis of their product type name. Used
+	 * to order stocks alphabetically by type name.
+	 * 
+	 * @author Alexandra
+	 * 
+	 */
+	public static class StockComparator implements Comparator<Stock>{
+
+		public int compare(Stock s1, Stock s2){
+			String t1 = s1.getType().getName();
+			String t2 = s2.getType().getName();
+
+			if (t1 == null && t2 == null){
+				return 0;
+			} else if (t1 == null){
+				return -1;
+			} else if (t2 == null){
+				return 1;
+			} else{
+				return t1.toLowerCase().compareTo(t2.toLowerCase());
+			}
+
+		}
+	}
+
 }
